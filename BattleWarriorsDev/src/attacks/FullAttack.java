d68bd81ef36ad9f8696abbf04cc7f0612266e2fd
chrisdemonte <chris.demonte@yahoo.com>
2019-06-23 22:31:37 -0400

    Started writing many of the attack classes.
    The doDamage methods still need to be writen.

---
+++ BattleWarriorsDev/src/attacks/FullAttack.java
@@ -0,0 +1,40 @@
+package attacks;
+
+import models.BaseStats;
+
+public class FullAttack implements Damage{
+	
+	String name;
+	String description;
+	
+	double physicalPower;
+	double magicPower;
+	double bonusDamage;
+	int energyCost;
+	int uses;
+	int currentUses;
+	int comboPointGain;
+	int comboPointRequirement;
+	
+	double accuracy;
+	double avoidability;
+	double crit;
+	double penetration; 
+	double time;
+	
+	Buff intialSelf;
+	Buff intialTarget;
+	Buff periodicSelf;
+	Buff periodicTarget;
+	Buff finalSelf;
+	Buff finalTarget;
+	
+	BaseStats requirements;
+
+	@Override
+	public void doDamage(Character self, Character target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ BattleWarriorsDev/src/attacks/MagicHeal.java
@@ -0,0 +1,36 @@
+package attacks;
+
+import models.BaseStats;
+
+public class MagicHeal implements Damage{
+	
+	String name;
+	String description;
+	double healingPower;
+	double bonusHealing; 
+	int energyCost;
+	int uses;
+	int currentUses;
+	int comboPointGain;
+	int comboPointRequirement;
+	double accuracy;
+	double crit;
+	double time;
+	
+	Buff intialSelf;
+	Buff intialTarget;
+	Buff periodicSelf;
+	Buff periodicTarget;
+	Buff finalSelf;
+	Buff finalTarget;
+	
+	BaseStats requirements;
+
+
+	@Override
+	public void doDamage(Character self, Character target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
